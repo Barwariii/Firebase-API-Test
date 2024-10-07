9a7947b0bfa50d643ae6b46f1e0c818a64fe27bd
Bilal Mohammed <kurdo1ka@gmail.com>
2024-10-07 15:23:45 +0200

    added a Object to Firebase and console.log it as a element or object

--- remoteStorage.js
+++ remoteStorage.js
@@ -1,13 +1,13 @@
 function onloadFunc() {
     console.log("test");
-    loadData();
+    loadData("/name"); // to Log a element we have give a path and to Log a object will be without a path loadData(""); 
 }
 
 
 const BASE_URL = "https://remotestorage-300dd-default-rtdb.europe-west1.firebasedatabase.app/";
 
-async function loadData() {
-   let response = await fetch(BASE_URL + ".json");
+async function loadData(path="") {
+   let response = await fetch(BASE_URL+ path + ".json");
    let responseToJson = await response.json();
    console.log(responseToJson);
    
