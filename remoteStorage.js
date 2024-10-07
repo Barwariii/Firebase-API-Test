a034fce321240fec2da21ef285d412f614a0fe15
Bilal Mohammed <kurdo1ka@gmail.com>
2024-10-07 15:16:27 +0200

    console Log Firebase data

--- remoteStorage.js
+++ remoteStorage.js
@@ -1,4 +1,14 @@
 function onloadFunc() {
     console.log("test");
-    
+    loadData();
+}
+
+
+const BASE_URL = "https://remotestorage-300dd-default-rtdb.europe-west1.firebasedatabase.app/";
+
+async function loadData() {
+   let response = await fetch(BASE_URL + ".json");
+   let responseToJson = await response.json();
+   console.log(responseToJson);
+   
 }
