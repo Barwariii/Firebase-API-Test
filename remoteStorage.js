d51d5419cff98e98dcc54149c2076e444a1f68d9
Bilal Mohammed <kurdo1ka@gmail.com>
2024-10-07 15:34:52 +0200

    added post fatch Function, added element to database

--- remoteStorage.js
+++ remoteStorage.js
@@ -1,6 +1,7 @@
 function onloadFunc() {
     console.log("test");
     loadData("/name"); // to Log a element we have give a path and to Log a object will be without a path loadData(""); 
+    postData("/name", {"banana": "rama"});
 }
 
 
@@ -12,4 +13,14 @@ async function loadData(path="") {
 //    let responseToJson = await response.json();
 //    console.log(responseToJson);
    
+}
+async function postData(path="", data={}) {
+    let response = await fetch(BASE_URL+ path + ".json",{
+        method: "POST",
+        header: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data)
+    });
+    return responseToJson = await response.json();
 }
